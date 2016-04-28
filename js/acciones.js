c35fcdd1cfffbe6ad05bbdb8e95599cf5a6a889d
karinarojas <karina3f.dsaupoo@gmail.com>
2016-04-28 14:24:43 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,7 @@
 $(document).ready(function(e){
 	document.addEventListener('deviceready',function(){
 		$('#btnvibrar').on('tap', function (){
-			navigator.notification.vibrate(2000);
+			navigator.notification.vibrate(3000);
 		});
 	});
 });
